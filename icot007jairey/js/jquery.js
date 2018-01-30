a7b6e0d15c28c31255b6a34b39f8c997f72536c4
Charlie Merrill <fcharliemerrill@gmail.com>
2018-01-30 14:03:05 -0800

    adds second milestone design

--- icot007jairey/js/jquery.js
+++ icot007jairey/js/jquery.js
@@ -1,53 +1,35 @@
 $(document).ready(function(){
   var n = 1;
-  $('.spingear-container').click(function(){
-    $('.spin-gear').removeClass('stop');
-    $('.spin-gear').addClass('rotated');
-    if (n == 5) {
-      $('.milestones').removeClass('milestone5');
-      $('.milestones').addClass('milestone1');
+  $('.milestone-container').click(function(){
+    $('.milestone-gear').removeClass('stop');
+    if (n == 3) {
+      $('.milestone-gear').removeClass('milestone3');
+      $('.milestone-gear').addClass('milestone1');
       n = 1;
-      } else if (n == 4) {
-      $('.milestones').removeClass('milestone4');
-      $('.milestones').addClass('milestone5');
-      n += 1;
-    } else if (n == 3) {
-      $('.milestones').removeClass('milestone3');
-      $('.milestones').addClass('milestone4');
-      n += 1;
     } else if (n == 2) {
-      $('.milestones').removeClass('milestone2');
-      $('.milestones').addClass('milestone3');
+      $('.milestone-gear').removeClass('milestone2');
+      $('.milestone-gear').addClass('milestone3');
       n += 1;
     } else if (n == 1) {
-      $('.milestones').removeClass('milestone1');
-      $('.milestones').addClass('milestone2');
+      $('.milestone-gear').removeClass('milestone1');
+      $('.milestone-gear').addClass('milestone2');
       n += 1;
     }
   });
 
-  $('.spingear-container').mouseup(function(){
-    $('.spin-gear').removeClass('rotated');
-    $('.spin-gear').addClass('zero-degree');
+  $('.milestone-container').mouseenter(function(){
+    $('.milestone-gear').removeClass('spin');
+    $('.milestone-gear').addClass('stop');
+    $('.milestone-gear').addClass('milestone1');
   });
 
-  $('.spingear-container').mouseenter(function(){
-    $('.spin-gear').removeClass('spin');
-    $('.spin-gear').addClass('stop');
-    $('.milestones').removeClass('slideshow');
-    $('.milestones').addClass('milestone1');
-  });
-
-  $('.spingear-container').mouseleave(function(){
-    $('.spin-gear').removeClass('rotated');
-    $('.spin-gear').removeClass('stop');
-    $('.spin-gear').addClass('spin');
-    $('.milestones').removeClass('milestone1');
-    $('.milestones').removeClass('milestone2');
-    $('.milestones').removeClass('milestone3');
-    $('.milestones').removeClass('milestone4');
-    $('.milestones').removeClass('milestone5');
-    $('.milestones').addClass('slideshow');
+  $('.milestone-container').mouseleave(function(){
+    $('.milestone-gear').removeClass('rotated');
+    $('.milestone-gear').removeClass('stop');
+    $('.milestone-gear').addClass('spin');
+    $('.milestone-gear').removeClass('milestone1');
+    $('.milestone-gear').removeClass('milestone2');
+    $('.milestone-gear').removeClass('milestone3');
     n = 1;
   });
 });
